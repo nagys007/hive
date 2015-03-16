2ed5841f34513f03c3c14920c237c24de40bd816
nagys007 <nagys007@gmail.com>
2015-03-16 13:27:56 +0100

    Initial version

---
+++ hiveudf/src/com/acme/hive/mynvl.java
@@ -0,0 +1,98 @@
+package com.acme.hive;
+
+/*
+ *
+ hive
+ add jar hiveudf-0.0.1-SNAPSHOT.jar;
+ create temporary function mynvl AS 'de.metafinanz.sny.hiveudf.mynvl';
+ describe function mynvl;
+ describe function extended mynvl;
+ SELECT myNVL(1,2) as a, myNVL(null,2) as b, 
+ myNVL('c','string') as c, myNVL(null,'string') as d,
+ myNVL(from_utc_timestamp('1972-08-29 00:00:00','CET'),from_unixtime(unix_timestamp())) as e,
+ myNVL(null,from_unixtime(unix_timestamp())) as e,
+ myNVL(1,2.0) as g, myNVL(1,'conversion') as h 
+ FROM dual;
+ Fails:
+ SELECT myNVL(1) FROM dual;
+ SELECT myNVL(1,2,3) FROM dual;
+
+ */
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDFUtils;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+
+@Description(name = "nvl", value = "_FUNC_ .. return the second argument, if the first is null,"
+		+ " otherwise it returns the first argument", extended = "_FUNC_(arg1, arg2)\n"
+		+ "Examples:\n"
+		+ "SELECT _FUNC_(1,2) FROM src; .. returns 1\n"
+		+ "SELECT _FUNC_(null,2) FROM src; .. returns 2\n"
+		+ "SELECT _FUNC_(null,'string or date or any other datatype possible too') FROM src;\n")
+public class mynvl extends GenericUDF {
+
+	private GenericUDFUtils.ReturnObjectInspectorResolver returnOIResolver;
+	private ObjectInspector[] argumentOIs;
+
+	@Override
+	public ObjectInspector initialize(ObjectInspector[] arguments)
+			throws UDFArgumentException {
+
+		if (arguments.length != 2) {
+			throw new UDFArgumentException("The function "
+					+ this.getClass().getName() + " requires two arguments.");
+		}
+
+		returnOIResolver = new GenericUDFUtils.ReturnObjectInspectorResolver(
+				true);
+		argumentOIs = arguments;
+
+		if (!(returnOIResolver.update(arguments[0]) && returnOIResolver
+				.update(arguments[0]))) {
+			throw new UDFArgumentException("The arguments of the function "
+					+ this.getClass().getName()
+					+ " must have the same data type,"
+					+ " but they are different: " + arguments[0].getTypeName()
+					+ " and " + arguments[1].getTypeName() + ".");
+		}
+		return returnOIResolver.get();
+	}
+
+	@Override
+	public Object evaluate(DeferredObject[] arguments) throws HiveException {
+		/*
+		 * code from Book "programming Hive" does not work. java Exception with
+		 * kryo ... Object retVal =
+		 * returnOIResolver.convertIfNecessary(arguments[0].get(),
+		 * argumentOIs[0]); if (retVal == null) { retVal =
+		 * returnOIResolver.convertIfNecessary(arguments[1].get(),
+		 * argumentOIs[1]); } return retVal; .. using
+		 * https://github.com/nexr/hive
+		 * -udf/blob/master/src/main/java/com/nexr/platform
+		 * /hive/udf/GenericUDFNVL.java .... but the same :(
+		 */
+		Object returnValue = null;
+		if (arguments[0].get() == null) {
+			// fieldValue is null, return defaultValue
+			returnValue = returnOIResolver.convertIfNecessary(
+					arguments[1].get(), argumentOIs[1]);
+		} else {
+			returnValue = returnOIResolver.convertIfNecessary(
+					arguments[0].get(), argumentOIs[0]);
+		}
+		return returnValue;
+		// return new Integer(0);
+	}
+
+	@Override
+	public String getDisplayString(String[] children) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("if ");
+		sb.append(children[0]);
+		sb.append(" is null returns ");
+		sb.append(children[1]);
+		return sb.toString();
+	}
+}
---
+++ hiveudf/src/com/acme/hive/zodiac.java
@@ -0,0 +1,149 @@
+package com.acme.hive;
+
+/*
+ * 
+ hive
+ add jar hiveudf-0.0.1-SNAPSHOT.jar;
+ create temporary function zodiac AS 'de.metafinanz.sny.hiveudf.zodiac';
+ describe function zodiac;
+ describe function extended zodiac; 
+ SELECT zodiac(8,29) FROM src;
+ SELECT zodiac('1972-08-29') FROM dual;
+ SELECT zodiac(to_date('1972-08-29 00:00:00')) FROM dual; -- processed as String
+ SELECT zodiac(to_date(from_unixtime(unix_timestamp()))) FROM dual;
+ SELECT zodiac(from_utc_timestamp('1972-08-29 00:00:00','CET')) FROM dual; -- processed as Date
+ SELECT zodiac(from_unixtime(unix_timestamp())) FROM dual;
+ */
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.exec.Description;
+
+@Description(name = "zodiac", value = "_FUNC_ .. return the sign of the Zodiac", extended = "_FUNC_(date)\n"
+		+ "_FUNC_(string) -> string format:yyyy-MM-dd\n"
+		+ "_FUNC_(month,day)\n"
+		+ "Examples:\n"
+		+ "SELECT _FUNC_(to_date('1972-08-29')) FROM src;\n"
+		+ "SELECT _FUNC_('1972-08-29') FROM src;\n"
+		+ "SELECT _FUNC_(8,29) FROM src;\n")
+public class zodiac extends UDF {
+
+	private SimpleDateFormat df;
+
+	private final String Capricorn = "Capricorn";
+	private final String Aquarius = "Aquarius";
+	private final String Pisces = "Pisces";
+	private final String Aries = "Aries";
+	private final String Taurus = "Taurus";
+	private final String Gemini = "Gemini";
+	private final String Cancer = "Cancer";
+	private final String Leo = "Leo";
+	private final String Virgo = "Virgo";
+	private final String Libra = "Libra";
+	private final String Scorpio = "Scorpio";
+	private final String Sagittarius = "Sagittarius";
+
+	public zodiac() {
+		df = new SimpleDateFormat("yyyy-MM-dd");
+	}
+
+	public String evaluate(String bdays) {
+		Date d = null;
+		try {
+			d = df.parse(bdays);
+		} catch (Exception e) {
+			return null;
+		}
+		return this.evaluate(d);
+	}
+
+	public String evaluate(Date bday) {
+		//return bday.toString();
+		Calendar c = Calendar.getInstance();
+		c.setTime(bday);
+		//return (c.get(Calendar.MONTH)+1) + " " + c.get(Calendar.DAY_OF_MONTH);*/
+		return this.evaluate(c.get(Calendar.MONTH) + 1, c.get(Calendar.DAY_OF_MONTH));
+	}
+
+	public String evaluate(Integer month, Integer day) {
+		switch (month) {
+		case 1:
+			if (day <= 20) {
+				return Capricorn;
+			} else {
+				return Aquarius;
+			}
+		case 2:
+			if (day <= 19) {
+				return Aquarius;
+			} else {
+				return Pisces;
+			}
+		case 3:
+			if (day <= 20) {
+				return Pisces;
+			} else {
+				return Aries;
+			}
+		case 4:
+			if (day <= 20) {
+				return Aries;
+			} else {
+				return Taurus;
+			}
+		case 5:
+			if (day <= 21) {
+				return Taurus;
+			} else {
+				return Gemini;
+			}
+		case 6:
+			if (day <= 21) {
+				return Gemini;
+			} else {
+				return Cancer;
+			}
+		case 7:
+			if (day <= 22) {
+				return Cancer;
+			} else {
+				return Leo;
+			}
+		case 8:
+			if (day <= 22) {
+				return Leo;
+			} else {
+				return Virgo;
+			}
+		case 9:
+			if (day <= 23) {
+				return Virgo;
+			} else {
+				return Libra;
+			}
+		case 10:
+			if (day <= 23) {
+				return Libra;
+			} else {
+				return Scorpio;
+			}
+		case 11:
+			if (day <= 22) {
+				return Scorpio;
+			} else {
+				return Sagittarius;
+			}
+		case 12:
+			if (day <= 21) {
+				return Sagittarius;
+			} else {
+				return Capricorn;
+			}
+		default:
+			return null;
+		}
+
+	}
+}
