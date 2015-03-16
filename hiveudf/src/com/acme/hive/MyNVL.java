a15e874b4f3b6806bf7091bb285a1c8307502f29
bik14sn <slavomir.nagy@metafinanz.de>
2015-03-16 08:14:19 -0700

    Problem with NullPointerException solved

--- hiveudf/src/com/acme/hive/MyNVL.java
+++ hiveudf/src/com/acme/hive/MyNVL.java
@@ -22,11 +22,17 @@ package com.acme.hive;
  DESCRIBE FUNCTION EXTENDED MyNVL;
  SELECT myNVL(1,2) as a FROM dual;
  SELECT myNVL(null,2) as b FROM dual;
- 
+SELECT myNVL('c','string') as c FROM dual;
+SELECT myNVL(null,'string') as d FROM dual;
+SELECT myNVL(from_utc_timestamp('1972-08-29 00:00:00','CET'),from_utc_timestamp(from_unixtime(unix_timestamp()),'CET')) as e FROM dual;
+SELECT myNVL(null,from_unixtime(unix_timestamp())) as f FROM dual;
+SELECT myNVL(1,2.0) as g FROM dual;
+SELECT myNVL(1,'conversion') as h FROM dual;
+
  SELECT myNVL(1,2) as a, myNVL(null,2) as b, 
  myNVL('c','string') as c, myNVL(null,'string') as d,
- myNVL(from_utc_timestamp('1972-08-29 00:00:00','CET'),from_unixtime(unix_timestamp())) as e,
- myNVL(null,from_unixtime(unix_timestamp())) as e,
+ myNVL(from_utc_timestamp('1972-08-29 00:00:00','CET'),from_utc_timestamp(from_unixtime(unix_timestamp()),'CET')) as e
+ myNVL(null,from_unixtime(unix_timestamp())) as f,
  myNVL(1,2.0) as g, myNVL(1,'conversion') as h 
  FROM dual;
  Fails:
@@ -36,10 +42,13 @@ package com.acme.hive;
  */
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException; 
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDFUtils;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 
 @Description(name = "nvl", value = "_FUNC_ .. return the second argument, if the first is null,"
 		+ " otherwise it returns the first argument", extended = "_FUNC_(arg1, arg2)\n"
@@ -56,24 +65,35 @@ public class MyNVL extends GenericUDF {
 	public ObjectInspector initialize(ObjectInspector[] arguments)
 			throws UDFArgumentException {
 
+		argumentOIs = arguments;
+		
 		if (arguments.length != 2) {
-			throw new UDFArgumentException("The function "
+			throw new UDFArgumentLengthException("Function "
 					+ this.getClass().getName() + " requires two arguments.");
 		}
 
+		if (arguments[0].getCategory() != ObjectInspector.Category.PRIMITIVE
+				|| arguments[1].getCategory() != ObjectInspector.Category.PRIMITIVE)
+				throw new UDFArgumentTypeException(0,
+				"Function "
+					+ this.getClass().getName() + " accepts only primitive data types.");
+
 		returnOIResolver = new GenericUDFUtils.ReturnObjectInspectorResolver(
 				true);
-/*		argumentOIs = arguments;
-
-		if (!(returnOIResolver.update(arguments[0]) && returnOIResolver
-				.update(arguments[0]))) {
+		
+        if (!(returnOIResolver.update(arguments[0]) 
+				&& returnOIResolver.update(arguments[1]))) {
 			throw new UDFArgumentException("The arguments of the function "
 					+ this.getClass().getName()
 					+ " must have the same data type,"
 					+ " but they are different: " + arguments[0].getTypeName()
 					+ " and " + arguments[1].getTypeName() + ".");
-		}*/
+		}
+		
+		
 		return returnOIResolver.get();
+		//return PrimitiveObjectInspectorFactory.javaIntObjectInspector;
+		
 	}
 
 	@Override
@@ -89,7 +109,9 @@ public class MyNVL extends GenericUDF {
 		 * -udf/blob/master/src/main/java/com/nexr/platform
 		 * /hive/udf/GenericUDFNVL.java .... but the same :(
 		 */
+		
 		Object returnValue = null;
+		
 		if (arguments[0].get() == null) {
 			// fieldValue is null, return defaultValue
 			returnValue = returnOIResolver.convertIfNecessary(
@@ -99,7 +121,7 @@ public class MyNVL extends GenericUDF {
 					arguments[0].get(), argumentOIs[0]);
 		}
 		return returnValue;
-		// return new Integer(0);
+		//return new Integer(42);
 	}
 
 	@Override
